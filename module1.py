2522cca2c8a1f945b1a4c824d450054f11facf5f
kalirocket <kalirocketdev@yahoo.com>
2020-05-18 10:51:21 -0500

    Shifting Characters

---
+++ module1.py
@@ -0,0 +1,7 @@
+# shift elements in string or even list
+def password_generator(txt):
+  something = ""
+  for i in range(0,len(txt)):
+    password += username[i-index] # enter the index
+  return something
+print(password_generator("henry"))
